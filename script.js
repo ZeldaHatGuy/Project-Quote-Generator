c3a5b64224db699d916c036a8f8a3633f6358f6a
gksingl <greg.k.singletary@gmail.com>
2022-02-05 09:55:39 -0600

    sending up finished product

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
+// Get Quotes From API
 'use strict';
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 
+let apiQuotes
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = !quote.author ? "Unknown": quote.author
+
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+
+    }
+    quoteText.textContent = quote.text;
+} 
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote()
+
+    } catch (error){
+
+    }
+}
+
+
+// tweet quote
+
+
+function tweetQuote() {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+
+
+// Even Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote )
+
+
+
+// On Load
+
+getQuotes();
 
 
